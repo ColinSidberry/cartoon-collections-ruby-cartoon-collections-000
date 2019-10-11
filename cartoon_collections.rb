954a278dc39e4c9833c5ebfebfd89909743e8f39
Colin Sidberry <colinsidberry@gmail.com>
2019-10-11 17:13:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,14 @@
 #1__________________________________________
 def roll_call_dwarves(array)
-  array.each.with_index do |ele,i|
-    puts (i+1).to_s + ". " + ele
-  end
+  i = array.length/2
+  while i < array.length
+    puts (i+1).to_s + ". " + array[i]
+    i += 1
+  end  
+  
+  # array.each.with_index do |ele,i|
+  #   puts (i+1).to_s + ". " + ele
+  # end
 end
 #2__________________________________________
 def summon_captain_planet(array)
@@ -19,11 +25,21 @@ def long_planeteer_calls(array)
 end
 #4__________________________________________
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = ["cheddar", "gouda", "camembert", "swiss"]
   array.each do |food|
     if cheese_types.include?(food)
       return food
     end
   end
   return nil
+end
+#5__________________________________________
+def word_with_b(words)
+  final_array = []
+  words.each do |word|
+    if word[0].downcase == "b"
+      final_array << word
+    end
+  end
+  return final_array
 end
